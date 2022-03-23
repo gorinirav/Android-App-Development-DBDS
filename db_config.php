b9106d9201c8ebd1299d89e5d81a62f00155c577
gorinirav <gorinirav@gmail.com>
2022-03-22 22:42:38 -0400

    Port added

--- db_config.php
+++ db_config.php
@@ -3,6 +3,7 @@ $servername = "rddb.chl55zwgoe4a.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "us-east-1.console";
 $dbname = "tetstdb";
+$port = "3306";
 
 // Create connection
 $link = mysqli_connect($servername, $username, $password, $dbname);
